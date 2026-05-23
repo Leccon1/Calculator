b311582954c5826e5b2365124faf571abf3a74e7
Leccon1 <dlavania703@gamil.com>
2026-05-24 00:58:17 +0300

    feat(Calculator): group number and helper buttons into block layout

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,25 @@ function App() {
 						<button className={styles.button}>/</button>
 						<button className={styles.button}>*</button>
 					</div>
-					<div className={`${styles.buttonNumbers} ${styles.container}`}>
-						<button className={styles.button}>1</button>
-						<button className={styles.button}>2</button>
-						<button className={styles.button}>3</button>
-						<button className={styles.button}>4</button>
-						<button className={styles.button}>5</button>
-						<button className={styles.button}>6</button>
-						<button className={styles.button}>7</button>
-						<button className={styles.button}>8</button>
-						<button className={styles.button}>9</button>
-						<button className={styles.button}>0</button>
-						<button className={styles.button}>.</button>
+					<div className={styles.block}>
+						<div className={`${styles.buttonNumbers} ${styles.container}`}>
+							<button className={styles.button}>1</button>
+							<button className={styles.button}>2</button>
+							<button className={styles.button}>3</button>
+							<button className={styles.button}>4</button>
+							<button className={styles.button}>5</button>
+							<button className={styles.button}>6</button>
+							<button className={styles.button}>7</button>
+							<button className={styles.button}>8</button>
+							<button className={styles.button}>9</button>
+							<button className={styles.button}>0</button>
+							<button className={styles.button}>.</button>
+						</div>
+						<div className={`${styles.buttonHelpers} ${styles.container}`}>
+							<button className={styles.button}>-</button>
+							<button className={styles.button}>+</button>
+							<button className={styles.button}>=</button>
+						</div>
 					</div>
 				</div>
 			</div>
