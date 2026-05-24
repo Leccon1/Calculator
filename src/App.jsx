48a0a95cd97ab87465455c5a8a3d8ac76da11d43
Leccon1 <dlavania703@gamil.com>
2026-05-25 00:14:25 +0300

    feat(Calculator): add data-type and data-value attributes to all buttons

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,20 @@ function App() {
 			: setExpression(expression + value)
 	}
 
+	const handleOperator = operator => {
+		if (isCalculated === true) {
+			operator === '='
+				? setExpression(expression)
+				: setExpression(expression + operator)
+		}
+
+		expression === '' && operator === '-'
+			? setExpression(operator)
+			: setExpression(expression + operator)
+
+		expression.length[-1] === operator ? expression.slice(0, -1) : expression
+	}
+
 	return (
 		<div className={styles.App}>
 			<div className={styles.calculator}>
@@ -26,41 +40,125 @@ function App() {
 				</div>
 				<div className={styles.buttonsContainer}>
 					<div className={`${styles.buttonHelpers} ${styles.container}`}>
-						<button className={styles.button}>Ac</button>
-						<button className={styles.button}>&#8592;</button>
-						<button className={`${styles.button} ${styles['button--accent']}`}>
+						<button data-type='clear' className={styles.button}>
+							Ac
+						</button>
+						<button data-type='delete' className={styles.button}>
+							&#8592;
+						</button>
+						<button
+							data-type='operator'
+							data-value='÷'
+							className={`${styles.button} ${styles['button--accent']}`}
+						>
 							/
 						</button>
-						<button className={`${styles.button} ${styles['button--accent']}`}>
+						<button
+							data-type='operator'
+							data-value='*'
+							className={`${styles.button} ${styles['button--accent']}`}
+						>
 							&#215;
 						</button>
 					</div>
 					<div className={styles.block}>
 						<div className={`${styles.buttonNumbers} ${styles.container}`}>
-							<button className={styles.button}>7</button>
-							<button className={styles.button}>8</button>
-							<button className={styles.button}>9</button>
-							<button className={styles.button}>4</button>
-							<button className={styles.button}>5</button>
-							<button className={styles.button}>6</button>
-							<button className={styles.button}>1</button>
-							<button className={styles.button}>2</button>
-							<button className={styles.button}>3</button>
-							<button className={styles.button}>0</button>
-							<button className={styles.button}>.</button>
+							<button
+								data-type='number'
+								data-value='7'
+								className={styles.button}
+							>
+								7
+							</button>
+							<button
+								data-type='number'
+								data-value='8'
+								className={styles.button}
+							>
+								8
+							</button>
+							<button
+								data-type='number'
+								data-value='9'
+								className={styles.button}
+							>
+								9
+							</button>
+							<button
+								data-type='number'
+								data-value='4'
+								className={styles.button}
+							>
+								4
+							</button>
+							<button
+								data-type='number'
+								data-value='5'
+								className={styles.button}
+							>
+								5
+							</button>
+							<button
+								data-type='number'
+								data-value='6'
+								className={styles.button}
+							>
+								6
+							</button>
+							<button
+								data-type='number'
+								data-value='1'
+								className={styles.button}
+							>
+								1
+							</button>
+							<button
+								data-type='number'
+								data-value='2'
+								className={styles.button}
+							>
+								2
+							</button>
+							<button
+								data-type='number'
+								data-value='3'
+								className={styles.button}
+							>
+								3
+							</button>
+							<button
+								data-type='number'
+								data-value='0'
+								className={styles.button}
+							>
+								0
+							</button>
+							<button
+								data-type='number'
+								data-value='.'
+								className={styles.button}
+							>
+								.
+							</button>
 						</div>
 						<div className={`${styles.buttonHelpers} ${styles.container}`}>
 							<button
+								data-type='operator'
+								data-value='-'
 								className={`${styles.button} ${styles['button--accent']}`}
 							>
 								-
 							</button>
 							<button
+								data-type='operator'
+								data-value='+'
 								className={`${styles.button} ${styles['button--accent']}`}
 							>
 								+
 							</button>
 							<button
+								data-type='evaluate'
+								data-value='='
 								className={`${styles.button} ${styles['button--equals']} ${styles['button--accent']}`}
 							>
 								=
